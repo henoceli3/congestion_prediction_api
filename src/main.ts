2fe42bc4f99cf088295eb9dec251f9fe678ccdd0
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2025-05-18 12:59:36 +0000

    fix(main): mettre à jour le message de démarrage pour refléter le port d'écoute correct

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(4000);
-  console.log(`Application is running on: http://localhost:3000`);
+  console.log(`Application is running on: http://localhost:4000`);
 }
 bootstrap();
